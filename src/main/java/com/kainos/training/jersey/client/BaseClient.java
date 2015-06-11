be3702c37a03aa7ac909e0a0b12cea125bc5e234
Damien Coney <d.coney@kainos.com>
2015-06-11 15:00:54 +0100

    Initial commit of sample client for training

---
+++ src/main/java/com/kainos/training/jersey/client/BaseClient.java
@@ -0,0 +1,34 @@
+package com.kainos.training.jersey.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientResponse;
+
+public class BaseClient 
+{
+
+	public Boolean getLogin(String username, String password){
+		Client client = ClientBuilder.newClient();
+		ClientConfig register = new ClientConfig();
+		WebTarget webTarget = client.target("http://localhost:8080");
+		WebTarget loginWebTarget = webTarget.path("login");		
+		Form form = new Form();
+		form.param("username", username);
+		form.param("password", password);
+		Invocation.Builder invocationBuilder = loginWebTarget.request();
+		Response response = invocationBuilder.post(Entity.entity(form, "application/x-www-form-urlencoded"));
+		if (response.getStatus() == 204){
+			return true;
+		}
+		throw new RuntimeException("Failed:" + response.getStatus());		
+	}
+}
---
+++ src/test/java/com/kainos/training/jersey/client/AppTest.java
@@ -0,0 +1,13 @@
+package com.kainos.training.jersey.client;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest{
+	
+    public AppTest( String testName )
+    {}
+
+    public void testApp(){}
+}
