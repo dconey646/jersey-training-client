a37fb951aaddce9cc521e7dc03c47c8f2540ed92
Andrew Wright <a.wright@kainos.com>
2015-08-10 18:07:38 +0100

    Renamed LoginClient from BaseClient and refactored to make it more readable

--- src/test/java/com/kainos/training/jersey/client/LoginClientTest.java
+++ src/test/java/com/kainos/training/jersey/client/LoginClientTest.java
@@ -11,27 +11,27 @@ import static org.hamcrest.core.Is.is;
 
 public class LoginTest{
 	
-	private BaseClient baseClient;
+	private LoginClient loginClient;
 	private static final String CORRECT_USERNAME = "admin";
 	private static final String CORRECT_PASSWORD = "password";
 	private static final String INCORRECT_USERNAME = "not admin";
 	private static final String INCORRECT_PASSWORD = "not password";
 	
 	@Before
-	public void setup(){
-		baseClient = new BaseClient();
+	public void setup() {
+		loginClient = new LoginClient();
 	}
 
 	@Test
-    public void shouldSuccessfullyLoginWithRightCredentials(){		
-		assertThat(baseClient.getLogin(CORRECT_USERNAME, CORRECT_PASSWORD), is(true));
+    public void shouldSuccessfullyLoginWithRightCredentials() {
+		assertThat(loginClient.getLogin(CORRECT_USERNAME, CORRECT_PASSWORD), is(true));
 	}
 	
 	@Test
-	public void shouldFailLoginWithIncorrectCredentials(){
+	public void shouldFailLoginWithIncorrectCredentials() {
 		try {
-			baseClient.getLogin(INCORRECT_USERNAME, INCORRECT_PASSWORD);
-		}catch(WebApplicationException e) {
+			loginClient.getLogin(INCORRECT_USERNAME, INCORRECT_PASSWORD);
+		} catch (WebApplicationException e) {
 			assertThat(e.getResponse().getStatus(), is(500));
 		}
 	}
